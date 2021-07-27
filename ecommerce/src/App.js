de85091b0880117fd5ce9971035ab50b7aa306cf
iceweasel <johnsjosephk@protonmail.com>
2021-07-27 23:15:16 +0530

    react-router-dom , {Switch } added

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,22 @@
-//import './App.css';
+import './App.css';
 import React from 'react' ;
+import { Route ,Switch} from 'react-router';
 import HomePage from './pages/homepage/homepage.component' ;
 import './pages/homepage/homepage.styles.scss';
+
+const HatsPage = () => (
+ <div>
+   <h1>HATS PAGE</h1>
+ </div>
+);
+
 function App() {
   return (
-    <div >
-        <HomePage />
+    <div>
+      <Switch>
+      <Route  exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
