b828c994af7cd77f45679266de8a93593eb1cfab
iceweasel <johnsjosephk@protonmail.com>
2021-08-01 23:58:59 +0530

    header component added with links to home ,shop

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -4,12 +4,12 @@ import { Route ,Switch } from 'react-router';
 import HomePage from './pages/homepage/homepage.component' ;
 import ShopPage from './pages/shop/shop.component.jsx';
 import './pages/homepage/homepage.styles.scss';
- 
-  
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route  exact path='/' component={HomePage} />
       <Route   path='/shop' component={ShopPage} />
