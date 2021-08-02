c90ba8e057659626f7a9b9cb3a4b719bcc3588fc
iceweasel <johnsjosephk@protonmail.com>
2021-08-02 12:17:00 +0530

    sign in form markup done

--- ecommerce/src/components/sign-in/sign-in.component.jsx
+++ ecommerce/src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FormInput from "../form-input/form-input.component";
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -26,19 +27,19 @@ class SignIn extends React.Component{
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form action="" onSubmit={this.handleSubmit}>
-                    <input name="email" 
+                    <FormInput name="email" 
                     type="email" 
-                    onChange= {this.handleChange}
+                    handleChange= {this.handleChange}
+                    label="Email"
                     value={this.state.email} required/>
-                    
-                    <label>Email</label>
-                    <input 
+
+                    <FormInput 
                     name="password" 
                     type="password" 
-                    onChange= {this.handleChange}
+                    handleChange= {this.handleChange}
+                    label="Password"
                     value={this.state.password} required/>
-                    <label>Password</label>
-
+ 
                     <input type="submit" value='submit form' />
                 </form>
             </div>
