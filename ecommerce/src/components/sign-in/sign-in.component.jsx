6c3daf7b669f7d520ef50ab95af498f77c6f3a8c
iceweasel <johnsjosephk@protonmail.com>
2021-08-02 17:09:46 +0530

    reusable custom button component created

--- ecommerce/src/components/sign-in/sign-in.component.jsx
+++ ecommerce/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -40,7 +41,7 @@ class SignIn extends React.Component{
                     label="Password"
                     value={this.state.password} required/>
  
-                    <input type="submit" value='submit form' />
+                    <CustomButton type="submit">Sign in </CustomButton>
                 </form>
             </div>
         )
