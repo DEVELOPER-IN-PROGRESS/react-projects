2fbed6f529832e1304302540ee1cf7b46ea7edfd
iceweasel <johnsjosephk@protonmail.com>
2021-08-02 11:26:13 +0530

    added sign in component and sign in page

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component' ;
 import ShopPage from './pages/shop/shop.component.jsx';
 import './pages/homepage/homepage.styles.scss';
 import Header from './components/header/header.component';
-
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 function App() {
   return (
     <div>
@@ -13,6 +13,7 @@ function App() {
       <Switch>
       <Route  exact path='/' component={HomePage} />
       <Route   path='/shop' component={ShopPage} />
+      <Route   path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
