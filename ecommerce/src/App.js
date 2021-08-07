11cc447deda885069ec1a0d2611e1e5540941589
iceweasel <johnsjosephk@protonmail.com>
2021-08-07 23:22:15 +0530

    finished sign in handleSubmit method

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -33,11 +33,11 @@ userRef.onSnapshot( snapShot => {
       ...snapShot.data()
     } 
   }) ;
-  console.log(this.state) ;
+ // console.log(this.state) ;
 }) ; 
    }
 
-   
+
    this.setState({ currentUser :userAuth })  ; 
    });
  }
