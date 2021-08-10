37554abdb3593541f6a2960ce13198763a8a0613
iceweasel <johnsjosephk@protonmail.com>
2021-08-10 23:48:48 +0530

    implemented cart reducer redux

--- ecommerce/src/components/header/header.component.jsx
+++ ecommerce/src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import  { ReactComponent as Logo } from '../../assets/crown.svg' ;
 import {auth} from '../../firebase/firebase.utils' ;
 import './header.styles.scss' ;
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser ,hidden}) => (
    <div className="header">
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -27,13 +27,17 @@ const Header = ({currentUser}) => (
          }
          <CartIcon />
          </div>
-         <CartDropdown hidden />
+         {
+          hidden ? null :
+         <CartDropdown  />
+         }
    </div>
    
 );
 
-const mapStateToProps = (state) => ({
-    currentUser : state.user.currentUser 
+const mapStateToProps = ({user : { currentUser } , cart: {hidden }}) => ({
+    currentUser ,
+    hidden 
 }) ;
 
 export default connect(mapStateToProps)(Header) ;
