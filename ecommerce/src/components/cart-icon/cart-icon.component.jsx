9c10ab310f61e88004de52ef67d627642934133a
iceweasel <johnsjosephk@protonmail.com>
2021-08-10 23:14:28 +0530

    cart dropdown  component

--- ecommerce/src/components/cart-icon/cart-icon.component.jsx
+++ ecommerce/src/components/cart-icon/cart-icon.component.jsx
@@ -5,7 +5,7 @@ import './cart-icon.styles.scss' ;
 const CartIcon = () => (
     <div className="cart-icon">
         <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">1</span>
+        <span className="item-count">0</span>
     </div>
 )
 
--- ecommerce/src/components/header/header.component.jsx
+++ ecommerce/src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux'; 
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import  { ReactComponent as Logo } from '../../assets/crown.svg' ;
 import {auth} from '../../firebase/firebase.utils' ;
 import './header.styles.scss' ;
@@ -26,6 +27,7 @@ const Header = ({currentUser}) => (
          }
          <CartIcon />
          </div>
+         <CartDropdown hidden />
    </div>
    
 );
