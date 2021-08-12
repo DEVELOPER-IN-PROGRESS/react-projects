585f843a8b53253b46d47570f5455079edd4ce0c
iceweasel <johnsjosephk@protonmail.com>
2021-08-12 21:47:15 +0530

    memoized every selector , replaced state with createStructuredSelector

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react' ;
 import { Route ,Switch ,Redirect } from 'react-router';
 import { connect } from 'react-redux'; 
+import { createStructuredSelector } from 'reselect';
 
 import HomePage from './pages/homepage/homepage.component' ;
 import ShopPage from './pages/shop/shop.component.jsx';
@@ -9,7 +10,8 @@ import './pages/homepage/homepage.styles.scss';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth , createUserProfileDocument} from './firebase/firebase.utils' ;
-import { setCurrentUser } from './redux/user/user.actions'
+import { setCurrentUser } from './redux/user/user.actions' ; 
+import { selectCurrentUser} from './redux/user/user.selectors' ; 
 class App extends React.Component {
   
 
@@ -58,8 +60,8 @@ userRef.onSnapshot( snapShot => {
   
 }
 
-const mapStateToProps  = ({user}) => ({
-  currentUser: user.currentUser 
+const mapStateToProps  = createStructuredSelector({
+  currentUser: selectCurrentUser
 }) ; 
 
 const mapDispatchToProps = dispatch => ({
