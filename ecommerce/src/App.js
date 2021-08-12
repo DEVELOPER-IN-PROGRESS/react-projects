eca9e64d0616f4ddebcf3fe3d8835d3fea32f7a2
iceweasel <johnsjosephk@protonmail.com>
2021-08-12 23:08:45 +0530

    checkout page

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -9,6 +9,7 @@ import ShopPage from './pages/shop/shop.component.jsx';
 import './pages/homepage/homepage.styles.scss';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import {auth , createUserProfileDocument} from './firebase/firebase.utils' ;
 import { setCurrentUser } from './redux/user/user.actions' ; 
 import { selectCurrentUser} from './redux/user/user.selectors' ; 
@@ -52,6 +53,7 @@ userRef.onSnapshot( snapShot => {
         <Switch>
         <Route  exact path='/' component={HomePage} />
         <Route   path='/shop' component={ShopPage} />
+        <Route exact  path='/checkout' component={CheckoutPage} />        
         <Route exact   path='/signin' render={ () => this.props.currentUser ? (<Redirect to='/' />): (<SignInAndSignUpPage />) } />
         </Switch>
       </div>
