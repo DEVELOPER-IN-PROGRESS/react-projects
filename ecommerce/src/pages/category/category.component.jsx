80eac25396cf84dc2936d71e8baa62add11fb232
iceweasel <johnsjosephk@protonmail.com>
2021-09-11 22:53:08 +0530

    nested routed in shop category page

---
+++ ecommerce/src/pages/category/category.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import collectionItemComponent from "../../components/collection-item/collection-item.component";
+
+import './category.styles.scss' ;
+
+const CategoryPage = ({match}) => {
+    console.log(match.params.CategoryId) ;
+    return(
+    <div className="category">
+        <h2>CATEGORY PAGE</h2>
+    </div>
+)
+} ;
+
+
+export default CategoryPage ;
